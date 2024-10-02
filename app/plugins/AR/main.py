db34df69f6f03b04c253764fd7ef9ce0ee033f82
Glas42 <145870870+Glas42@users.noreply.github.com>
2024-10-02 21:01:00 +0200

    Renamed variables in AR

--- app/plugins/AR/main.py
+++ app/plugins/AR/main.py
@@ -40,10 +40,10 @@ def InitializeWindow():
                     ("cyTopHeight", ctypes.c_int),
                     ("cyBottomHeight", ctypes.c_int)]
 
-    hwnd = win32gui.FindWindow(None, 'ETS2LA - AR/Overlay')
-    margins = MARGINS(-1, -1, -1, -1)
-    ctypes.windll.dwmapi.DwmExtendFrameIntoClientArea(hwnd, margins)
-    win32gui.SetWindowLong(hwnd, win32con.GWL_EXSTYLE, win32gui.GetWindowLong(hwnd, win32con.GWL_EXSTYLE) | win32con.WS_EX_LAYERED | win32con.WS_EX_TRANSPARENT)
+    HWND = win32gui.FindWindow(None, 'ETS2LA - AR/Overlay')
+    Margins = MARGINS(-1, -1, -1, -1)
+    ctypes.windll.dwmapi.DwmExtendFrameIntoClientArea(HWND, Margins)
+    win32gui.SetWindowLong(HWND, win32con.GWL_EXSTYLE, win32gui.GetWindowLong(HWND, win32con.GWL_EXSTYLE) | win32con.WS_EX_LAYERED | win32con.WS_EX_TRANSPARENT)
 
 
 def Resize():
@@ -131,113 +131,125 @@ def CalculateAlpha(Distances=()):
         return 0
 
 
-def ConvertToScreenCoordinate(x: float, y: float, z: float):
-    head_yaw = head_rotation_degrees_x
-    head_pitch = head_rotation_degrees_y
-    head_roll = head_rotation_degrees_z
+def ConvertToScreenCoordinate(X: float, Y: float, Z: float):
+    HeadYaw = HeadRotationDegreesX
+    HeadPitch = HeadRotationDegreesY
+    HeadRoll = HeadRotationDegreesZ
 
-    rel_x = x - head_x
-    rel_y = y - head_y
-    rel_z = z - head_z
+    RelativeX = X - HeadX
+    RelativeY = Y - HeadY
+    RelativeZ = Z - HeadZ
 
-    cos_yaw = math.cos(math.radians(-head_yaw))
-    sin_yaw = math.sin(math.radians(-head_yaw))
-    new_x = rel_x * cos_yaw + rel_z * sin_yaw
-    new_z = rel_z * cos_yaw - rel_x * sin_yaw
+    CosYaw = math.cos(math.radians(-HeadYaw))
+    SinYaw = math.sin(math.radians(-HeadYaw))
+    NewX = RelativeX * CosYaw + RelativeZ * SinYaw
+    NewZ = RelativeZ * CosYaw - RelativeX * SinYaw
 
-    cos_pitch = math.cos(math.radians(-head_pitch))
-    sin_pitch = math.sin(math.radians(-head_pitch))
-    new_y = rel_y * cos_pitch - new_z * sin_pitch
-    final_z = new_z * cos_pitch + rel_y * sin_pitch
+    CosPitch = math.cos(math.radians(-HeadPitch))
+    SinPitch = math.sin(math.radians(-HeadPitch))
+    NewY = RelativeY * CosPitch - NewZ * SinPitch
+    FinalZ = NewZ * CosPitch + RelativeY * SinPitch
 
-    cos_roll = math.cos(math.radians(head_roll))
-    sin_roll = math.sin(math.radians(head_roll))
-    final_x = new_x * cos_roll - new_y * sin_roll
-    final_y = new_y * cos_roll + new_x * sin_roll
+    CosRoll = math.cos(math.radians(HeadRoll))
+    SinRoll = math.sin(math.radians(HeadRoll))
+    FinalX = NewX * CosRoll - NewY * SinRoll
+    FinalY = NewY * CosRoll + NewX * SinRoll
 
-    if final_z >= 0:
+    if FinalZ >= 0:
         return None, None, None
 
-    fov_rad = math.radians(variables.FOV)
+    FovRad = math.radians(variables.FOV)
     
-    window_distance = ((WindowPosition[3] - WindowPosition[1]) * (4 / 3) / 2) / math.tan(fov_rad / 2)
+    WindowDistance = ((WindowPosition[3] - WindowPosition[1]) * (4 / 3) / 2) / math.tan(FovRad / 2)
 
-    screen_x = (final_x / final_z) * window_distance + (WindowPosition[2] - WindowPosition[0]) / 2
-    screen_y = (final_y / final_z) * window_distance + (WindowPosition[3] - WindowPosition[1]) / 2
+    ScreenX = (FinalX / FinalZ) * WindowDistance + (WindowPosition[2] - WindowPosition[0]) / 2
+    ScreenY = (FinalY / FinalZ) * WindowDistance + (WindowPosition[3] - WindowPosition[1]) / 2
 
-    screen_x = (WindowPosition[2] - WindowPosition[0]) - screen_x
+    ScreenX = (WindowPosition[2] - WindowPosition[0]) - ScreenX
 
-    distance = math.sqrt((rel_x ** 2) + (rel_y ** 2) + (rel_z ** 2))
+    Distance = math.sqrt((RelativeX ** 2) + (RelativeY ** 2) + (RelativeZ ** 2))
 
-    return screen_x, screen_y, distance
+    return ScreenX, ScreenY, Distance
 
 
-def Run(data):
+def Run(Data):
     global LastWindowPosition
     global WindowPosition
 
-    global head_rotation_degrees_x
-    global head_rotation_degrees_y
-    global head_rotation_degrees_z
-    global head_x
-    global head_y
-    global head_z
+    global HeadRotationDegreesX
+    global HeadRotationDegreesY
+    global HeadRotationDegreesZ
+    global HeadX
+    global HeadY
+    global HeadZ
 
-    data = {}
-    data["api"] = TruckSimAPI.update()
+    Data = {}
+    Data["api"] = TruckSimAPI.update()
 
     WindowPosition = ScreenCapture.GetWindowPosition(Name="Truck Simulator", Blacklist=["Discord"])
     if LastWindowPosition != WindowPosition:
         Resize()
 
 
-    truck_x = data["api"]["truckPlacement"]["coordinateX"]
-    truck_y = data["api"]["truckPlacement"]["coordinateY"]
-    truck_z = data["api"]["truckPlacement"]["coordinateZ"]
-    truck_rotation_x = data["api"]["truckPlacement"]["rotationX"]
-    truck_rotation_y = data["api"]["truckPlacement"]["rotationY"]
-    truck_rotation_z = data["api"]["truckPlacement"]["rotationZ"]
+    TruckX = Data["api"]["truckPlacement"]["coordinateX"]
+    TruckY = Data["api"]["truckPlacement"]["coordinateY"]
+    TruckZ = Data["api"]["truckPlacement"]["coordinateZ"]
+    TruckRotationX = Data["api"]["truckPlacement"]["rotationX"]
+    TruckRotationY = Data["api"]["truckPlacement"]["rotationY"]
+    TruckRotationZ = Data["api"]["truckPlacement"]["rotationZ"]
 
-    cabin_offset_x = data["api"]["headPlacement"]["cabinOffsetX"] + data["api"]["configVector"]["cabinPositionX"]
-    cabin_offset_y = data["api"]["headPlacement"]["cabinOffsetY"] + data["api"]["configVector"]["cabinPositionY"]
-    cabin_offset_z = data["api"]["headPlacement"]["cabinOffsetZ"] + data["api"]["configVector"]["cabinPositionZ"]
-    cabin_offset_rotation_x = data["api"]["headPlacement"]["cabinOffsetrotationX"]
-    cabin_offset_rotation_y = data["api"]["headPlacement"]["cabinOffsetrotationY"]
-    cabin_offset_rotation_z = data["api"]["headPlacement"]["cabinOffsetrotationZ"]
+    CabinOffsetX = Data["api"]["headPlacement"]["cabinOffsetX"] + Data["api"]["configVector"]["cabinPositionX"]
+    CabinOffsetY = Data["api"]["headPlacement"]["cabinOffsetY"] + Data["api"]["configVector"]["cabinPositionY"]
+    CabinOffsetZ = Data["api"]["headPlacement"]["cabinOffsetZ"] + Data["api"]["configVector"]["cabinPositionZ"]
+    CabinOffsetRotationX = Data["api"]["headPlacement"]["cabinOffsetrotationX"]
+    CabinOffsetRotationY = Data["api"]["headPlacement"]["cabinOffsetrotationY"]
+    CabinOffsetRotationZ = Data["api"]["headPlacement"]["cabinOffsetrotationZ"]
 
-    head_offset_x = data["api"]["headPlacement"]["headOffsetX"] + data["api"]["configVector"]["headPositionX"] + cabin_offset_x
-    head_offset_y = data["api"]["headPlacement"]["headOffsetY"] + data["api"]["configVector"]["headPositionY"] + cabin_offset_y
-    head_offset_z = data["api"]["headPlacement"]["headOffsetZ"] + data["api"]["configVector"]["headPositionZ"] + cabin_offset_z
-    head_offset_rotation_x = data["api"]["headPlacement"]["headOffsetrotationX"]
-    head_offset_rotation_y = data["api"]["headPlacement"]["headOffsetrotationY"]
-    head_offset_rotation_z = data["api"]["headPlacement"]["headOffsetrotationZ"]
+    HeadOffsetX = Data["api"]["headPlacement"]["headOffsetX"] + Data["api"]["configVector"]["headPositionX"] + CabinOffsetX
+    HeadOffsetY = Data["api"]["headPlacement"]["headOffsetY"] + Data["api"]["configVector"]["headPositionY"] + CabinOffsetY
+    HeadOffsetZ = Data["api"]["headPlacement"]["headOffsetZ"] + Data["api"]["configVector"]["headPositionZ"] + CabinOffsetZ
+    HeadOffsetRotationX = Data["api"]["headPlacement"]["headOffsetrotationX"]
+    HeadOffsetRotationY = Data["api"]["headPlacement"]["headOffsetrotationY"]
+    HeadOffsetRotationZ = Data["api"]["headPlacement"]["headOffsetrotationZ"]
 
-    truck_rotation_degrees_x = truck_rotation_x * 360
-    truck_rotation_radians_x = -math.radians(truck_rotation_degrees_x)
+    TruckRotationDegreesX = TruckRotationX * 360
+    TruckRotationRadiansX = -math.radians(TruckRotationDegreesX)
 
-    head_rotation_degrees_x = (truck_rotation_x + cabin_offset_rotation_x + head_offset_rotation_x) * 360
-    while head_rotation_degrees_x > 360:
-        head_rotation_degrees_x = head_rotation_degrees_x - 360
+    HeadRotationDegreesX = (TruckRotationX + CabinOffsetRotationX + HeadOffsetRotationX) * 360
+    while HeadRotationDegreesX > 360:
+        HeadRotationDegreesX = HeadRotationDegreesX - 360
 
-    head_rotation_degrees_y = (truck_rotation_y + cabin_offset_rotation_y + head_offset_rotation_y) * 360
+    HeadRotationDegreesY = (TruckRotationY + CabinOffsetRotationY + HeadOffsetRotationY) * 360
 
-    head_rotation_degrees_z = (truck_rotation_z + cabin_offset_rotation_z + head_offset_rotation_z) * 360
+    HeadRotationDegreesZ = (TruckRotationZ + CabinOffsetRotationZ + HeadOffsetRotationZ) * 360
 
-    point_x = head_offset_x
-    point_y = head_offset_y
-    point_z = head_offset_z
-    head_x = point_x * math.cos(truck_rotation_radians_x) - point_z * math.sin(truck_rotation_radians_x) + truck_x
-    head_y = point_y * math.cos(math.radians(head_rotation_degrees_y)) - point_z * math.sin(math.radians(head_rotation_degrees_y)) + truck_y
-    head_z = point_x * math.sin(truck_rotation_radians_x) + point_z * math.cos(truck_rotation_radians_x) + truck_z
+    PointX = HeadOffsetX
+    PointY = HeadOffsetY
+    PointZ = HeadOffsetZ
+    HeadX = PointX * math.cos(TruckRotationRadiansX) - PointZ * math.sin(TruckRotationRadiansX) + TruckX
+    HeadY = PointY * math.cos(math.radians(HeadRotationDegreesY)) - PointZ * math.sin(math.radians(HeadRotationDegreesY)) + TruckY
+    HeadZ = PointX * math.sin(TruckRotationRadiansX) + PointZ * math.cos(TruckRotationRadiansX) + TruckZ
 
 
-    x1, y1, d1 = ConvertToScreenCoordinate(x=10448.742, y=35.324, z=-10132.315)
+    TruckWheelPointsX = [Point for Point in Data["api"]["configVector"]["truckWheelPositionX"]]
+    TruckWheelPointsY = [Point for Point in Data["api"]["configVector"]["truckWheelPositionY"]]
+    TruckWheelPointsZ = [Point for Point in Data["api"]["configVector"]["truckWheelPositionZ"]]
 
-    x2, y2, d2 = ConvertToScreenCoordinate(x=10453.237, y=36.324, z=-10130.404)
+    for i in range(len(TruckWheelPointsX)):
+        PointX = TruckWheelPointsX[i] + TruckX
+        PointY = TruckWheelPointsY[i] + TruckY
+        PointZ = TruckWheelPointsZ[i] + TruckZ
+        X, Y, D = ConvertToScreenCoordinate(X=PointX, Y=PointY, Z=PointZ)
+        DrawCircle(X=X, Y=Y, R=10, Color=(255, 255, 255), FillColor=(127, 127, 127, 127), Thickness=2)
 
-    x3, y3, d3 = ConvertToScreenCoordinate(x=10453.237, y=34.324, z=-10130.404)
-    
-    Alpha = CalculateAlpha(Distances=(d1, d2, d3))
+
+    X1, Y1, D1 = ConvertToScreenCoordinate(X=10448.742, Y=35.324, Z=-10132.315)
+
+    X2, Y2, D2 = ConvertToScreenCoordinate(X=10453.237, Y=36.324, Z=-10130.404)
+
+    X3, Y3, D3 = ConvertToScreenCoordinate(X=10453.237, Y=34.324, Z=-10130.404)
+
+    Alpha = CalculateAlpha(Distances=(D1, D2, D3))
 
 
     DrawRectangle(X1=0, Y1=0, X2=100, Y2=100, Color=(255, 255, 255), Thickness=2)
@@ -245,6 +257,6 @@ def Run(data):
     DrawPolygon(Points=[(125, 0), (125, 125), (0, 125)], Color=(255, 255, 255), FillColor=(0, 0, 0, 127), Thickness=2, Closed=False)
     DrawCircle(X=0, Y=0, R=100, Color=(255, 255, 255), FillColor=(0, 0, 0, 127), Thickness=2)
 
-    DrawPolygon(Points=[(x1, y1), (x2, y2), (x3, y3)], Color=(255, 255, 255, Alpha), FillColor=(127, 127, 127, Alpha / 2), Thickness=2, Closed=True)
+    DrawPolygon(Points=[(X1, Y1), (X2, Y2), (X3, Y3)], Color=(255, 255, 255, Alpha), FillColor=(127, 127, 127, Alpha / 2), Thickness=2, Closed=True)
 
     Render()

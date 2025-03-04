f37d41b110e52a2f0acd6b14e5d9d15cf1f1001f
Glas42 <145870870+OleFranz@users.noreply.github.com>
2025-03-04 15:04:32 +0100

    fixed settings corrupting sometimes

--- app/src/settings.py
+++ app/src/settings.py
@@ -1,31 +1,60 @@
 import src.variables as variables
 import json
+import time
+import os
 
 
-def EnsureFile(File:str):
+def Lock():
     try:
-        with open(File, "r") as f:
-            try:
-                json.load(f)
-            except:
-                with open(File, "w") as ff:
-                    ff.write("{}")
+        Count = 0
+        while os.path.exists(f"{variables.Path}config/settings-lock.txt"):
+            time.sleep(0.001)
+            Count += 1
+            if Count > 100:
+                break
+        with open(f"{variables.Path}config/settings-lock.txt", "w") as File:
+            File.write("")
+        File.close()
     except:
-        with open(File, "w") as f:
-            f.write("{}")
+        pass
+
+
+def Free():
+    try:
+        if os.path.exists(f"{variables.Path}config/settings-lock.txt"):
+            os.remove(f"{variables.Path}config/settings-lock.txt")
+    except:
+        pass
+
+
+def EnsureFile(FileStr:str):
+    try:
+        with open(FileStr, "r") as File:
+            try:
+                json.load(File)
+            except:
+                with open(FileStr, "w") as FileFile:
+                    FileFile.write("{}")
+    except:
+        with open(File, "w") as File:
+            File.write("{}")
 
 
 def Get(Category:str, Name:str, Value:any=None):
     try:
+        Lock()
         EnsureFile(f"{variables.Path}config/settings.json")
-        with open(f"{variables.Path}config/settings.json", "r") as f:
-            Settings = json.load(f)
+        with open(f"{variables.Path}config/settings.json", "r") as File:
+            Settings = json.load(File)
+        File.close()
+        Free()
 
         if Settings[Category][Name] == None:
             return Value
 
         return Settings[Category][Name]
     except:
+        Free()
         if Value != None:
             Set(Category, Name, Value)
             return Value
@@ -35,9 +64,11 @@ def Get(Category:str, Name:str, Value:any=None):
 
 def Set(Category:str, Name:str, Data:any):
     try:
+        Lock()
         EnsureFile(f"{variables.Path}config/settings.json")
-        with open(f"{variables.Path}config/settings.json", "r") as f:
-            Settings = json.load(f)
+        with open(f"{variables.Path}config/settings.json", "r") as File:
+            Settings = json.load(File)
+        File.close()
 
         if not Category in Settings:
             Settings[Category] = {}
@@ -46,8 +77,10 @@ def Set(Category:str, Name:str, Data:any):
         if Category in Settings:
             Settings[Category][Name] = Data
 
-        with open(f"{variables.Path}config/settings.json", "w") as f:
-            f.truncate(0)
-            json.dump(Settings, f, indent=6)
+        with open(f"{variables.Path}config/settings.json", "w") as File:
+            File.truncate(0)
+            json.dump(Settings, File, indent=6)
+        File.close()
+        Free()
     except:
-        pass
+        Free()
